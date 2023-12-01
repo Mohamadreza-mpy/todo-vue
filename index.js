1319bea72fa9a50e8a37f5534421c37fa6e6c898
mohammadreza <mohammadreza.mir.8448@gmail.com>
2023-12-01 03:43:34 +0330

    add and delete task and don't add Repetitious task

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('dddd')
+new Vue({
+    el : '#app',
+    data:{
+        items:[],
+        item:null,
+    },
+    methods:{
+        addItems:function ()
+        {
+
+            if(this.item.trim() == "")
+            {
+                alert('تسک را وارد کنید');
+            }else
+            {
+
+                if(this.items.includes(this.item))
+                {
+                    alert('تکراری هست')
+                }else
+                {
+                    this.items.push(this.item)
+                }
+                this.item = ""
+            }
+
+        },
+        removeItem:function (index)
+        {
+
+            this.items.splice(index, 1)
+        }
+    },
+})
